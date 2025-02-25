20d3c3b5db6fad448cf8ab8c3fe9657bd4d126aa
Therk007 <tgvipkamisama@gmail.com>
2025-02-25 09:11:34 +0530

    Update letest.py

--- letest.py
+++ letest.py
@@ -161,7 +161,7 @@ def handle_attack(message):
 
     pending_feedback[user_id] = True  
 
-    full_command = f"./pushpa {target} {port} {time_duration} 800"
+    full_command = f"./bgmi {target} {port} {time_duration} 900"
 
     try:
         subprocess.run(full_command, shell=True, check=True)
